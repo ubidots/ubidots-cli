50e969b3d7916c91f188451bd04ae98f28ea2fda
Cristian Rubio <cristian.rubio@ubidots.com>
2024-10-07 14:41:41 +0000

    :hammer: [UF-58] Functions: Update default templates

--- cli/functions/handlers.py
+++ cli/functions/handlers.py
@@ -5,7 +5,9 @@ from cli.commons.utils import build_endpoint
 from cli.commons.utils import exit_with_error_message
 from cli.commons.utils import exit_with_success_message
 from cli.functions import FUNCTION_API_ROUTES
+from cli.functions.enums import FunctionLanguageEnum
 from cli.functions.helpers import build_functions_payload
+from cli.settings import settings
 
 
 def list_functions(fields: str, filter: str, sort_by: str, page_size: int, page: int):
@@ -40,12 +42,8 @@ def add_function(**kwargs):
     )
     client = httpx.Client(follow_redirects=True)
     response = client.post(url, headers=headers, json=data)
-    if response.status_code == httpx.codes.CREATED:
-        exit_with_success_message(
-            f"The function with 'id={response.json()['id']}' and "
-            f"'label={response.json()['label']}' was created successfully."
-        )
-    else:
+    response_json = response.json()
+    if response.status_code != httpx.codes.CREATED:
         exit_with_error_message(
             httpx.HTTPStatusError(
                 message=response._content.decode("utf-8"),
@@ -53,6 +51,39 @@ def add_function(**kwargs):
                 response=response,
             )
         )
+
+    runtime = data["serverless"]["runtime"]
+    language = FunctionLanguageEnum.get_language_by_runtime(runtime)
+    zip_path = settings.FUNCTIONS.TEMPLATES_PATH / f"{language}.zip"
+    with open(zip_path, "rb") as zip_ref:
+        zip_file = zip_ref.read()
+
+    url, headers = build_endpoint(
+        route=FUNCTION_API_ROUTES["zip_file"],
+        function_key=response_json["id"],
+    )
+    files = {
+        "zipFile": (
+            "function_file_code.zip",
+            zip_file,
+            "application/zip",
+        )
+    }
+
+    response = client.post(url=url, headers=headers, files=files)
+    if response.status_code != httpx.codes.OK:
+        exit_with_error_message(
+            httpx.HTTPStatusError(
+                message=response._content.decode("utf-8"),
+                request=response.request,
+                response=response,
+            )
+        )
+
+    exit_with_success_message(
+        f"The function with 'id={response_json['id']}' and "
+        f"'label={response_json['label']}' was created successfully."
+    )
 
 
 def update_function(function_key: str, **kwargs):
